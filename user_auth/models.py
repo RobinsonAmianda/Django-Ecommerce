3a946d589e30d1aa2bdc96769b8eaa5e61819c76
RobinsonAmianda <robinsonamianda5@gmail.com>
2025-01-18 16:34:37 +0300

    Admin_Auth

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+USER_TYPE = (
+    ('Vendor', 'Vendor'),
+    ('Customer', 'Customer'),
+)
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(unique=True)
+
+    # Replacing the username with email while logging in .
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email 
+    
+    # Saving all the User fields to the database
+    def save(self , *args, **kwargs):
+        email_username,_ = self.email.split('@')
+        if not self.username:
+            self.username = email_username 
+        super(User, self).save(*args,**kwargs)
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images' , default='default-user.jpg', blank=True, null=True)
+    fullname = models.CharField(max_length=200 , blank=True, null=True)
+    mobile = models.CharField(max_length=200 , blank=True, null=True)
+    user_Type= models.CharField(max_length=200 , choices =USER_TYPE, default=None , blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self , *args, **kwargs):
+        if not self.fullname:
+            self.fullname = self.user.username 
+        super(Profile, self).save(*args,**kwargs)
